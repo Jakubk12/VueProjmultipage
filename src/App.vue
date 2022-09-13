4b4e8ba14682ef12cd5634c384670edaad6fca88
Jakubk12 <jakubkeska11@gmail.com>
2022-09-13 13:30:52 +0200

    added basic files

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <the-header></the-header>
+<main-component></main-component>
+<the-footer></the-footer>
+</div>
+</template>
+<script>
+    import TheHeader from './components/layouts/TheHeader.vue';
+    import MainComponent from './components/MainComponent.vue';
+    import TheFooter from './components/layouts/TheFooter.vue';
+export default {
+  components: {TheHeader : TheHeader,
+    MainComponent : MainComponent,
+    TheFooter : TheFooter},
+
+  data()
+  {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
