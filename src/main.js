9fc4b90278f4ce567554b2fa3c3917dee84df47e
Jakubk12 <jakubkeska11@gmail.com>
2022-10-15 19:39:07 +0200

    Changes and preparing main.js to add simple router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import { createApp } from 'vue'
 import VWave from 'v-wave'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue'
 //import router from "./components/router/index.js"
 
-createApp(App)
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes: [],
+  });
+
+
+ const app = createApp(App)
 //.use(router)
-.use(VWave)
-.mount('#app')
+.use(VWave, router)
+app.mount('#app')
