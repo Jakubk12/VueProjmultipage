039bbb350b2e2407ea7f7979dc1ade6d0e763506
Jakubk12 <jakubkeska11@gmail.com>
2022-10-17 19:59:08 +0200

    Corrected styles of buttons

--- src/components/layouts/ListComponent.vue
+++ src/components/layouts/ListComponent.vue
@@ -1,32 +1,31 @@
 <template>
     <div class="header-buttons-menu">
-  <button v-wave="{ color: '#eca1a6', duration: 2, initialOpacity: 2, easing: 'ease-in',}" class="basic">Home</button> 
-  <button v-wave="{ color: '#eca1a6', duration: 2, initialOpacity: 2, easing: 'ease-in',}" class="basic">Today</button> 
-  <button v-wave="{ color: '#eca1a6', duration: 2, initialOpacity: 2, easing: 'ease-in',}" class="basic">About</button> 
+<router-link  :to="{ path: '/components/MainComponent'}"><button class="basic" v-wave="{ color: '#eca1a6', duration: 2, initialOpacity: 2, easing: 'ease-in',}">Home</button></router-link>
+<router-link  :to="{ path: '/components/Routes/SurveyCode'}"><button class="basic" v-wave="{ color: '#eca1a6', duration: 2, initialOpacity: 2, easing: 'ease-in',}">Survey</button></router-link>           
+<router-link  :to="{ path: '/components/Routes/Today'}"><button class="basic" v-wave="{ color: '#eca1a6', duration: 2, initialOpacity: 2, easing: 'ease-in',}">Today</button></router-link>           
   </div>
+  <router-view></router-view>
 </template>
 <script>
 
 </script>
 
-
-
-
 <style lang="scss" scoped>
-.header-buttons-menu {
+
+.header-buttons-menu {  
         height: 5%;
         width: 100%;
         margin: auto;
         top: 25%;
-        align-content: center;
         background-color: #3a0061;
-        display: flex;
-        font-family: 'Courier New', Courier, monospace;
 
+        display: inline-flex;
+        font-family: 'Courier New', Courier, monospace;
 .basic {
+    
+    display: inline-block;
     background: #6b5b95;
-  margin-right: 165px;
-  margin-left: 165px;
+  margin-left: 265px;
   margin-top: 25px;
   margin-bottom: 25px;
   text-align: center;
@@ -35,9 +34,7 @@
   font-size: 1em;
   color: white;
   cursor: pointer;
-  width: 100%;
-
-
+  width: 30%;
 
 &:hover
 {
@@ -49,46 +46,19 @@
 }
 }
 }
-@media all and (max-width:450px) {
-
-} 
-@media all and (max-width:600px) {
-    .header-buttons-menu {
-        width: 100%;
-        display: block;
-        margin-right: auto;
-   flex-basis: 80%;
-      padding-right: 1%;
-      .basic {
-        margin-left: auto;
-      }
-    }
-
-}
 
 @media all and (max-width:1000px) {
   .header-buttons-menu {
-    width: 100%;
     display: block;
-    flex-basis: 80%;
-    margin-left: auto;
-        margin-right: auto;
-        padding-right: 25px;
-        padding-left: 5px;
-
-
+        padding-left: 25px;
+        padding-right: 35px;
     .basic {
-                margin-left: auto;
-        margin-right: auto;
-
-     
-    }
-
-    
+        width: 30%;
+        margin-left: 5px;
+        margin-right: 5px;
+    } 
 }
   }
-
-
 @media all and (min-width:1000px) and (max-width: 1100px) {
   .header-buttons-menu {
         width: 100%;
@@ -97,12 +67,12 @@
         margin-left: auto;
         padding-right: 25px;
         padding-left: 5px;
-        .basic {
-            
-        }
-
-   
 }
 
+}
+@media all and (max-width:1500px)  {
+  .basic {
+    margin-left: 265px;
+  }
 }
 </style>
