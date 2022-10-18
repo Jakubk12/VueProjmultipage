e50f8ca8d8f1699c0ace106e9861a5213a360f81
Jakubk12 <jakubkeska11@gmail.com>
2022-10-18 16:35:09 +0200

    correction of styles in ListComponent.vue

--- src/components/layouts/ListComponent.vue
+++ src/components/layouts/ListComponent.vue
@@ -15,26 +15,23 @@
 .header-buttons-menu {  
         height: 5%;
         width: 100%;
-        margin: auto;
         top: 25%;
         background-color: #3a0061;
-
-        display: inline-flex;
+        padding-bottom: 10px;
+        padding-top: 10px;
+      
         font-family: 'Courier New', Courier, monospace;
 .basic {
     
-  display: inline-block;
   background: #6b5b95;
-  margin-left: 265px;
-  margin-top: 25px;
-  margin-bottom: 25px;
+  margin-left: 25px;
   text-align: center;
   padding: 1em;
   font-family: 'Source Sans Pro', sans-serif;
   font-size: 1em;
   color: white;
   cursor: pointer;
-  width: 30%;
+  width: 20%;
 
 &:hover
 {
@@ -59,20 +56,5 @@
     } 
 }
   }
-@media all and (min-width:1000px) and (max-width: 1100px) {
-  .header-buttons-menu {
-        width: 100%;
-        display: flex;
-        margin-right: auto;
-        margin-left: auto;
-        padding-right: 25px;
-        padding-left: 5px;
-}
 
-}
-@media all and (max-width:1500px)  {
-  .basic {
-    margin-left: 265px;
-  }
-}
 </style>
