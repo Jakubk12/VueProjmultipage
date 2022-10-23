e3fa25a958def4eaeaecd261d325f6deb53143a7
Jakubk12 <jakubkeska11@gmail.com>
2022-10-23 18:23:26 +0200

    button

---
+++ src/components/Routes/RoutesUI/BasicButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="basic">Confirm</button>
+</template>
+
+<style lang="scss" scoped >
+.basic {
+    cursor: pointer;
+    background-color:#6fba26;
+	padding:10px;
+	position:relative;
+	font-family: 'Open Sans', sans-serif;
+	font-size:12px;
+	text-decoration:none;
+	color:#fff;
+	background-image: linear-gradient(bottom, rgb(100,170,30) 0%, rgb(129,212,51) 100%);
+	box-shadow: inset 0px 1px 0px #b2f17f, 0px 6px 0px #3d6f0d;
+	border-radius: 5px;
+
+    &hover {
+        background-color: #3d6f0d;
+    }
+
+}
+</style>
+
